eff458ba738d93d720ebf755cb1cdf8f70856aab
nursofia09 <nur.sofia@mail.unpas.ac.id>
2016-12-23 15:28:58 +0700

    grafik report

---
+++ grafikreport/controllers/Grafikreport.php
@@ -0,0 +1,44 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Grafikreport extends MX_Controller {
+
+	public function __construct() {
+       parent::__construct();
+		$this->load->model('Mgrafik');
+    }
+
+	public function index() {
+		$data['mapel'] = $this->Mgrafik->getdaftarmapel();
+		$this->load->view('template/header');
+        $this->load->view('v-header');
+		$this->load->view('pilih_mapel', $data);
+	}
+
+	// fungsi pilihan bab
+    public function pilih_bab_report($no) {
+            $data['bab'] = $this->Mgrafik->get_mapel_bab($no);
+            $data['mapel'] = $no;
+            $this->load->view('template/header');
+            $this->load->view('v-header');
+            $this->load->view('bab_grafik', $data);
+    }
+
+    public function greport($bab) {
+    $data['report'] = $this->Mgrafik->get_greport($bab);
+    
+
+
+        // $this->load->view('template/header');
+        // $this->load->view('v-header');
+        // $this->load->view('Videoback/layout/header');
+        $this->load->view('v-greport', $data);
+    }
+
+
+
+
+	
+
+
+}
---
+++ grafikreport/models/Mgrafik.php
@@ -0,0 +1,61 @@
+<?php 
+
+/**
+* 
+*/
+class Mgrafik extends CI_Model
+{
+	// get mapel
+	public function getdaftarmapel(){
+    	$this->db->distinct();
+		$this->db->select()->from('tb_mata_pelajaran');
+		$this->db->where('status = 1');
+		$tampil=$this->db->get();
+		return $tampil->result_array();
+    }
+    // get mapel dan bab
+    public function get_mapel_bab($mapel) {
+        $this->db->distinct();
+        $this->db->select('m.id_mapel, m.nama_mapel, m.alias_mapel, b.id_mapel, b.id_bab, b.judul_bab');
+        $this->db->from('tb_mata_pelajaran m');
+        $this->db->join('tb_bab b', 'm.id_mapel = b.id_mapel');
+        $this->db->where('m.nama_mapel', $mapel);
+        // $this->db->select('id_bab, judul_bab, id_mapel');
+        // $this->db->from('tb_bab');
+        // $this->db->where('id_mapel', $mapel);
+         
+        $query = $this->db->get();
+        return $query->result_array();
+    }
+
+    public function get_greport($bab){
+        $query = "SELECT m.nama_mapel, b.judul_bab, ROUND((SUM( sub_score ) / sum(total)) * 100) AS score, SUM( total ) AS total 
+        FROM tb_grafik_report as g 
+        RIGHT JOIN tb_bab as b
+        ON  g.id_bab = b.id_bab
+        JOIN tb_mata_pelajaran as m
+        ON b.id_mapel = m.id_mapel
+        WHERE g.id_bab ='$bab'";
+
+        // $this->db->select('*, bab.judul_bab, pel.nama_mapel');
+        // $this->db->from('tb_latihan latihan');
+        // $this->db->join('tb_report_latihan report',
+        //     'latihan.id_latihan=report.id_latihan');
+        // $this->db->join('tb_bab bab',
+        //     'latihan.id_bab=bab.id_bab');
+        // $this->db->join('tb_mata_pelajaran pel',
+        //     'pel.id_mapel=bab.id_mapel');
+        // $this->db->where('create_by', $createdby);
+        // $this->db->where('bab.id_mapel', $mapel);
+        // $this->db->order_by('tgl_pengerjaan', 'asc');
+        // $query = $this->db->get();
+        $result = $this->db->query($query);
+        return $result->result_array();
+
+
+
+    }
+   
+}
+
+ ?>
